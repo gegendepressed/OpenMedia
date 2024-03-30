fde5c9c24b661945036b6a59dbdfbde2f0b33dab
gegendepressed <sairaj09pai@gmail.com>
2024-03-30 21:33:59 +0530

    initial commit

---
+++ form.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, EmailField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2,max=19)])
+    email = EmailField('Email Address', validators=[DataRequired(),Email()])
+    password = PasswordField('New Password', validators=[DataRequired()])
+    confirm_password = PasswordField("Repeat the Password",validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    email = EmailField('Email Address', validators=[DataRequired(),Email()])
+    password = PasswordField('New Password', validators=[DataRequired()] )
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Login")
