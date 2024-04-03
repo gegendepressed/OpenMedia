e0bc3d90e0c6ce85b9414789717054a62d7e0104
FrosT2k5 <androygaming@gmail.com>
2024-04-03 10:25:59 +0530

    app.py: remove redundant posts dict No longer needed
    Signed-off-by: FrosT2k5 <androygaming@gmail.com>

--- app.py
+++ app.py
@@ -14,21 +14,6 @@ with app.app_context():
 
 salt = "mysalt"
 
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
 
 @app.route("/")
 @app.route("/home")
